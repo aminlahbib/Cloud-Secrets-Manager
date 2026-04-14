b047de21f302dec45e58a67234e6a5293f2f090b
Amin Lahbib <amine.lhb00@gmail.com>
2026-04-14 04:18:51 +0200

    Create values-production.yaml with 3 replicas, cert-manager TLS, security headers, HPA

---
+++ infrastructure/terraform/environments/staging/provider.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_version = ">= 1.5"
+
+  backend "gcs" {
+    bucket = "cloud-secrets-manager-tfstate"
+    prefix = "terraform/staging"
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = var.skip_k8s_resources ? "https://unused" : "https://${module.gke.cluster_endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = var.skip_k8s_resources ? "" : base64decode(module.gke.cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = var.skip_k8s_resources ? "https://unused" : "https://${module.gke.cluster_endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = var.skip_k8s_resources ? "" : base64decode(module.gke.cluster_ca_certificate)
+  }
+}
